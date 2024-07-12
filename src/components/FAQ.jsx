b00b2b76b22c50b232af1af430b6b59ee1c9859d
Kaiser Mighty <unikmey@gmail.com>
2024-07-12 12:22:55 -0700

    FAQ Styling

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -3,11 +3,13 @@ import Accordion from './Accordion'
 
 const FAQ = () => {
     return (
-        <div id='faq' className='m-4 mb-0 p-4 bg-[var(--primary-dark)] rounded-lg'>
-            <h1 className='text-white font-bold p-2 flex justify-center'>Frequently Asked Questions</h1>
-            <Accordion question='Question 1' answer='Answer 1' />
-            <Accordion question='Question 2' answer='Answer 2' />
-            <Accordion question='Question 3' answer='Answer 3' />
+        <div id='faq' className='mt-4 max-w-[800px] mx-auto'>
+            <div className='bg-[var(--primary-dark)] rounded-lg mx-4 p-4'>
+                <h1 className='text-white font-bold p-2 flex justify-center'>Frequently Asked Questions</h1>
+                <Accordion question='Question 1' answer='Answer 1' />
+                <Accordion question='Question 2' answer='Answer 2' />
+                <Accordion question='Question 3' answer='Answer 3' />
+            </div>
         </div>
     )
 }
