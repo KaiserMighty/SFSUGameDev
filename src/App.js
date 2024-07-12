cc4699bc2155c4ca7ce0a40975b88b166be3566e
Kaiser Mighty <unikmey@gmail.com>
2024-07-12 09:50:52 -0700

    Merge top and nav bar, color scheme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import TopBar from './components/TopBar'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Activities from './components/Activities'
@@ -7,7 +6,6 @@ import Activities from './components/Activities'
 function App() {
   return (
     <div>
-      <TopBar />
       <NavBar />
       <Hero />
       <Activities />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import {GiRetroController} from 'react-icons/gi'
 import {
     FaFacebookF,
     FaTwitter,
@@ -15,27 +16,25 @@ const handleNav = () => {
 }
 
     return (
-        <div className='w-full min-h-[50px] flex justify-between items-center z-20 text-white bg-gray-700/80 sticky top-[46px]'>
+        <div className='w-full min-h-[50px] flex justify-between items-center px-4 py-2 z-20 text-white bg-[var(--primary-dark)] sticky top-0'>
+            <div className='flex items-center'>
+                <GiRetroController size={30} className='text-white mr-3' />
+                <h1 className='text-xl font-bold text-white'>GAME DEV @ SFSU</h1>
+            </div>
             <ul className='hidden sm:flex px-4'>
-                <li>
+                <li className='py-0'>
                     <a href='/'>Home</a>
                 </li>
-                <li>
+                <li className='py-0'>
                     <a href='#join'>Join</a>
                 </li>
-                <li>
+                <li className='py-0'>
                     <a href='#faq'>FAQ</a>
                 </li>
-                <li>
+                <li className='py-0'>
                     <a href='#directions'>Directions</a>
                 </li>
             </ul>
-            <div className='flex justify-between'>
-                <FaFacebookF className='mx-4' />
-                <FaTwitter className='mx-4' />
-                <FaGooglePlusG className='mx-4' />
-                <FaInstagram className='mx-4' />
-            </div>
             {/* Hamburger Icon */}
             <div onClick={handleNav} className='sm:hidden z-20'>
                 <FaBars size={20} className='mr-4 cursor-pointer' />
