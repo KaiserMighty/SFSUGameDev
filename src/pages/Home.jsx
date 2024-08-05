bac5a19c1febe72f8da61ee78284bd42687c5ea3
Kaiser Mighty <unikmey@gmail.com>
2024-08-04 20:01:13 -0700

    Add Directions to CS Lab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import NavBar from '../components/NavBar'
 import Hero from '../components/Hero'
 import Activities from '../components/Activities'
 import FAQ from '../components/FAQ'
+import Location from '../components/Location'
 import Footer from '../components/Footer'
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
         <Hero />
         <Activities />
         <FAQ />
+        <Location />
         <Footer />
     </div>
   );
