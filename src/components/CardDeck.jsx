d840b52a4acb71831b30d211476f7c4e1d08e6d5
Kaiser Mighty <unikmey@gmail.com>
2024-08-12 16:58:38 -0700

    Scalability Adjustment

--- src/components/CardDeck.jsx
+++ src/components/CardDeck.jsx
@@ -1,29 +1,20 @@
 import React from 'react'
 import Card from './Card'
 
-const CardDeck = () => {
+const CardDeck = ({content}) => {
     return (
         <div className='flex flex-col sm:min-h-[calc(100vh-10rem)] min-h-[calc(100vh-16rem)]'>
             <div className='flex-grow'>
                 <div className='grid grid-flow-row auto-rows-max grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-6 m-6'>
-                    <Card 
-                        image='https://img.itch.zone/aW1nLzE2MTY0MjQ3LnBuZw==/315x250%23c/Yvf9hQ.png'
-                        title='ReWired'
-                        lead='Alvin-Tulud'
-                        link='https://alvin-tulud.itch.io/rewired'
-                    />
-                    <Card 
-                        image='https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg'
-                        title='BUTCHERED'
-                        lead='lindseytong'
-                        link='https://lindseytong.itch.io/butchered'
-                    />
-                    <Card 
-                        image='https://img.itch.zone/aW1nLzE0MTk0MjEwLnBuZw==/315x250%23c/dzUVAR.png'
-                        title='PRJCTB'
-                        lead='OrangesAndCrema'
-                        link='https://orangesandcrema.itch.io/prjctb'
-                    />
+                    {content.map((innerArray, index) => (
+                        <Card 
+                            key={index}
+                            image={innerArray[0]}
+                            title={innerArray[1]}
+                            lead={innerArray[2]}
+                            link={innerArray[3]}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,7 +7,29 @@ function Projects() {
   return (
     <div>
         <NavBar />
-        <CardDeck />
+        <CardDeck
+          content=
+          {[
+            [
+              'https://img.itch.zone/aW1nLzE2MTY0MjQ3LnBuZw==/315x250%23c/Yvf9hQ.png',
+              'ReWired',
+              'Alvin-Tulud',
+              'https://alvin-tulud.itch.io/rewired'
+            ],
+            [
+              'https://www.russorizio.com/wp-content/uploads/2016/07/ef3-placeholder-image.jpg',
+              'BUTCHERED',
+              'lindseytong',
+              'https://lindseytong.itch.io/butchered'
+            ],
+            [
+              'https://img.itch.zone/aW1nLzE0MTk0MjEwLnBuZw==/315x250%23c/dzUVAR.png',
+              'PRJCTB',
+              'OrangesAndCrema',
+              'https://orangesandcrema.itch.io/prjctb'
+            ],
+          ]}
+        />
         <Footer />
     </div>
   );
