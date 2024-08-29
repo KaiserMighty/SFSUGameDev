f3fc5daf0aae4e485c10ee2e969d387b39180c0f
Kaiser Mighty <unikmey@gmail.com>
2024-08-28 22:47:34 -0700

    Images

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -6,7 +6,7 @@ const Activities = () => {
             <div className='relative p-4'>
                 <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Workshops</h3>
                 <img
-                    src="https://images.unsplash.com/photo-1720659201108-4efe526b289c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://i.imgur.com/gDX7i2G.jpeg"
                     alt="/"
                     className='w-full h-full object-cover relative border-4 border-white shadow-2xl'
                 />
@@ -14,7 +14,7 @@ const Activities = () => {
             <div className='relative p-4'>
                 <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Projects</h3>
                 <img
-                    src="https://images.unsplash.com/photo-1719931626206-d3874333fd00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://i.imgur.com/HutBk3z.jpeg"
                     alt="/"
                     className='w-full h-full object-cover relative border-4 border-white shadow-2xl'
                 />
@@ -22,7 +22,7 @@ const Activities = () => {
             <div className='relative p-4'>
                 <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Game Jams</h3>
                 <img
-                    src="https://images.unsplash.com/photo-1719742882558-c7c8c076b4fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://i.imgur.com/jdHbYcx.jpeg"
                     alt="/"
                     className='w-full h-full object-cover relative border-4 border-white shadow-2xl'
                 />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className='w-full h-[90vh]'>
             <img
-                src="https://images.unsplash.com/photo-1720597000771-6c936f9ca0e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://i.imgur.com/SPe1rs3.jpeg"
                 alt="/"
                 className='w-full h-full object-cover'
             />
